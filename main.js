79fd5de707918d6ee1ae93584bcb314becbadff7
TEHYA-coder <81315725+TEHYA-coder@users.noreply.github.com>
2022-10-21 20:39:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+song1 = ""
+song2 = ""
+
+function preload() {
+    song1 = loadSound("wholenewworld.mp3");
+    song2 = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image (video, 0, 0, 600, 500);
+}
